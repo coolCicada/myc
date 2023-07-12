119a16d3a6af776ed51f6faa77b4c3d6d4fdd79a
liangsai <liangsai@meituan.com>
2023-07-12 15:34:31 +0800

    feat:vue库依赖处理

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,11 @@ module.exports = {
   configureWebpack: {
     // plugins: [new BundleAnalyzerPlugin()],
     externals: {
-      vue: 'Vue' // Key 是 import 语句中使用的名字，值是浏览器全局环境中的名字
+      vue: {
+        commonjs: 'vue',
+        commonjs2: 'vue',
+        amd: 'vue',
+      },
     }
   },
 }
